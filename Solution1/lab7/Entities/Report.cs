a9fbed214360d38764f7f74c82b023787d40411f
Aptlos <dikovskiy.artem@gmail.com>
2023-12-18 18:18:49 +0200

    created entities for users and reports

---
+++ Solution1/lab7/Entities/Report.cs
@@ -0,0 +1,9 @@
+﻿namespace lab7.Entitites;
+
+public class Report
+{
+    public int idReports { get; set; }
+    public int Creator { get; set; }
+    public int Intruder { get; set; }
+    public int Reason { get; set; }
+}
---
+++ Solution1/lab7/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using lab7.Entitites;
+
+namespace lab7.Entities;
+
+public class User
+{
+    public int idUsers { get; set; }
+    public string UserName { get; set; }
+    public string UserRole { get; set; }
+    public IEnumerable<Report> Reports { get; set; }
+}
