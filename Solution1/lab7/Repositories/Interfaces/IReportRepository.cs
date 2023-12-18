9976bd4d2e04a56c5881d4a4988364db279bd2e3
Aptlos <dikovskiy.artem@gmail.com>
2023-12-18 18:27:15 +0200

    created interfaces for repositories

---
+++ Solution1/lab7/Repositories/Interfaces/IReportRepository.cs
@@ -0,0 +1,7 @@
+﻿using lab7.Entitites;
+
+namespace lab7.Repositories.Interfaces;
+
+public interface IReportRepository : IRepository<Report>
+{
+}
---
+++ Solution1/lab7/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using lab7.Entities;
+
+namespace lab7.Repositories.Interfaces;
+
+public interface IUserRepository : IRepository<User>
+{
+    
+}
