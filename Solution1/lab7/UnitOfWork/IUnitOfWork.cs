a36e6c8013e5e6a7fac631bc70aa218c1de679e7
Aptlos <dikovskiy.artem@gmail.com>
2023-12-19 00:14:47 +0200

    Added Unit of Work

---
+++ Solution1/lab7/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using lab7.Repositories.Interfaces;
+
+namespace lab7.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IReportRepository Reports { get; }
+    IUserRepository Users { get; }
+    void Save();
+}
