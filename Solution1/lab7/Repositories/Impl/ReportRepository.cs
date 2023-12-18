0750fde50406266af86c12828e759c5da1a71e6b
Aptlos <dikovskiy.artem@gmail.com>
2023-12-19 00:00:35 +0200

    Added Repositories for reports and users

---
+++ Solution1/lab7/Repositories/Impl/ReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using lab7.EF;
+using lab7.Entities;
+using lab7.Repositories.Interfaces;
+
+namespace lab7.Repositories.Impl;
+
+public class ReportRepository : BaseRepository<Report>, IReportRepository
+{
+    internal ReportRepository(ReportContext context)
+        : base(context)
+    {
+    }
+}
---
+++ Solution1/lab7/Repositories/Impl/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using lab7.EF;
+using lab7.Entities;
+using lab7.Repositories.Interfaces;
+
+namespace lab7.Repositories.Impl;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    internal UserRepository(ReportContext context)
+        : base(context)
+    {
+    }
+}
