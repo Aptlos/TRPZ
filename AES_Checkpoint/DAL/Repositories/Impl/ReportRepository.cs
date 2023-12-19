c0353441ec7a5fb9174864271b17011d3b1687b4
Aptlos <dikovskiy.artem@gmail.com>
2023-12-19 20:54:54 +0200

    Added Repositories

---
+++ AES_Checkpoint/DAL/Repositories/Impl/ReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.EF;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories.Impl;
+
+public class ReportRepository : BaseRepository<Report>, IReportRepository
+{
+    internal ReportRepository(ReportContext context)
+        : base(context)
+    {
+    }
+}
---
+++ AES_Checkpoint/DAL/Repositories/Impl/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.EF;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories.Impl;
+
+public class UserRepository : BaseRepository<User>, IUserRepository
+{
+    internal UserRepository(ReportContext context)
+        : base(context)
+    {
+    }
+}
