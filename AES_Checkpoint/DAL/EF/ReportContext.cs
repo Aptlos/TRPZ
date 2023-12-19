d310338a2b1acc5a1d15f47ee8ca8d2443d1e58e
Aptlos <dikovskiy.artem@gmail.com>
2023-12-19 20:52:53 +0200

    Added .Net EntityFrameworkCore and context for Data Base

---
+++ AES_Checkpoint/DAL/EF/ReportContext.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF;
+
+public class ReportContext : DbContext
+{
+    public DbSet<User> Phones { get; set; }
+    public DbSet<Report> Orders { get; set; }
+    public ReportContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+}
