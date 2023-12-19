c0901de6690f3b15fd06994f85b2ef82f3d6a7e7
Aptlos <dikovskiy.artem@gmail.com>
2023-12-19 21:29:28 +0200

    Added test-clone for repository and test for Create

---
+++ AES_Checkpoint/DAL.Tests/BaseRepositoryUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using DAL.EF;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Tests;
+using Moq;
+using Xunit;
+
+public class BaseRepositoryUnitTest
+{
+    [Fact]
+    public void Create_InputReportInstance_CalledAddMethodOfDBSetWithReportInstance()
+    {
+        // Arrange
+        DbContextOptions opt = new DbContextOptionsBuilder<ReportContext>().Options;
+        var mockContext = new Mock<ReportContext>(opt);
+        var mockDbSet = new Mock<DbSet<Report>>();
+        
+        mockContext.Setup(context => context.Set<Report>()).Returns(mockDbSet.Object);
+        var repository = new TestReportRepository(mockContext.Object);
+        
+        Report expectedReport = new Mock<Report>().Object;
+        
+        //Act
+        repository.Create(expectedReport);
+        
+        // Assert
+        mockDbSet.Verify(
+        dbSet => dbSet.Add(
+            expectedReport
+            ), Times.Once());
+    }
+}
---
+++ AES_Checkpoint/DAL.Tests/TestReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Tests;
+
+public class TestReportRepository : BaseRepository<Report>
+{
+    public TestReportRepository(DbContext context)
+        : base(context)
+    {
+    }
+}
