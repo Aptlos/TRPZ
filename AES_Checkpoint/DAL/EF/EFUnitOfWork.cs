a8809589ae0036dcfc07f214f072c770e16b2a03
Aptlos <dikovskiy.artem@gmail.com>
2023-12-19 20:56:38 +0200

    Added Unit of Work

---
+++ AES_Checkpoint/DAL/EF/EFUnitOfWork.cs
@@ -0,0 +1,66 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Impl;
+using DAL.Repositories.Interfaces;
+using DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF;
+
+public class EFUnitOfWork
+    : IUnitOfWork
+{
+    private ReportContext db;
+    private ReportRepository reportRepository;
+    private UserRepository userRepository;
+
+    public EFUnitOfWork(DbContextOptions options)
+    {
+        db = new ReportContext(options);
+    }
+
+    public IReportRepository Reports
+    {
+        get
+        {
+            if (reportRepository == null)
+                reportRepository = new ReportRepository(db);
+            return reportRepository;
+        }
+    }
+
+    public IUserRepository Users
+    {
+        get
+        {
+            if (userRepository == null)
+                userRepository = new UserRepository(db);
+            return userRepository;
+        }
+    }
+
+    public void Save()
+    {
+        db.SaveChanges();
+    }
+
+    private bool disposed = false;
+
+    public virtual void Dispose(bool disposing)
+    {
+        if (!this.disposed)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            this.disposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ AES_Checkpoint/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Repositories.Interfaces;
+
+namespace DAL.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IReportRepository Reports { get; }
+    IUserRepository Users { get; }
+    void Save();
+}
